f43365d424abb21fdead1cb835b66258feba9825
unknown <Centaurus73@proton.me>
2023-07-24 22:06:44 -0700

    set up the host to 0.0.0.0

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from flaskapp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
 
-    #app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
