b145fe3bbb8c7baf3ada2ed1f7ebf15231554f0e
DeathCookieOP <procookieop@gmail.com>
2023-07-30 11:18:21 -0700

    Created and set up database
    I created the database and set it up so it worked on the online hosting. I made one user inside the user table and 2 inside cart table under that user for testing

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thisissecretkey'
+app = Flask(__name__)
 
+def create_app():    
+    app.config['SECRET_KEY'] = 'thisissecretkey'       
+    
     from .views import views
     from .auth import auth
     from .products import products_blueprint
+    from .db import db_blueprint
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(products_blueprint)
+    app.register_blueprint(db_blueprint)
 
 
     return app
---
+++ flaskapp/db.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template
+from flask_login import current_user
+from flask_sqlalchemy import SQLAlchemy
+
+from . import app
+
+db_blueprint = Blueprint('db', __name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://sql9635613:aL1s9kYG4G@sql9.freemysqlhosting.net:3306/sql9635613'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+#initialize DB
+db = SQLAlchemy(app)
+app.app_context().push()
+
+#Models for Tables    
+class Users(db.Model):
+   id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+   email = db.Column(db.String(50), nullable = False, unique = True)
+        
+   def __repr__(self):
+        return f'<User: {self.id}, Email:{self.email}>'
+    
+class Cart(db.Model):
+    cart_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    brand = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<Cart Owner: {self.owner_id}, Brand+Name+Price: {self.brand} {self.name} {self.price}>'
+
+#Testing db by just finding a user and their cart
+@db_blueprint.route("/db")
+def db_route():
+    
+    # THIS CODE WORKS!
+        
+    # curr_user = db.get_or_404(Users, 5) #5 is the id of the Users (primary key)
+    curr_user = Users.query.filter(Users.email == "test@csun.com").first()
+    curr_user_cart = Cart.query.filter(Cart.owner_id == curr_user.id).all()
+        
+    return render_template("testdb.html", user = curr_user, user_cart = curr_user_cart)
+
+
+
