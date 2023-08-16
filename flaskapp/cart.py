d1577cebeccfc1c9ef20e185f7dd7c9df6f6e91e
Jorge Torrez <65319802+Thellorch@users.noreply.github.com>
2023-08-15 20:58:59 -0400

    Documentation for cart.py

--- flaskapp/cart.py
+++ flaskapp/cart.py
@@ -1,3 +1,19 @@
+"""
+Module name: cart.py
+Date:  08/04/2023
+Programmer's name: Jorge Torrez and Ronny Almahdi
+
+Description:
+This module is responsible for handling cart-related operations in the e-commerce application. 
+It includes routes for viewing the cart, updating cart items, and removing items from the cart.
+
+Important data structure:
+- Users and Cart models are utilized for querying and manipulating cart-related data in the database.
+
+Algorithms:
+- The update_cart() function uses a basic input validation to check if the user has provided a new quantity. 
+- The remove_item() function deletes a cart item from the database based on the provided cart_id.
+"""
 from flask import Blueprint, render_template, session, redirect, url_for, request
 from flask_login import current_user
 from flask import flash
@@ -5,21 +21,34 @@ from flask import flash
 from .db import db
 from .modules.models.user_model import Users
 from .modules.models.cart_model import Cart
-from .modules.models.product_model import Products
 
 cart_blueprint = Blueprint('cart', __name__)
 
 @cart_blueprint.route("/cart")
 def cart():
+    """
+    Function:
+    cart():
+    - Description: Fetches and displays the cart items for the logged-in user.
+    - Input: None
+    - Output: Redirects the user to the login page if not logged in, otherwise renders the cart page.
+    """
     curr_user = Users.get_user_by_email(session.get('user'))
     if curr_user == None:
         return redirect('/login')
     else:
         cart_items = curr_user.get_user_cart()
-        return render_template("cart.html", cart_items=cart_items, user=curr_user, products = Products.get_prod_list())
+        return render_template("cart.html", cart_items=cart_items, user=curr_user)
     
 @cart_blueprint.route("/update_cart/<int:cart_id>", methods=['GET', 'POST']) #the int will be the cart_id
 def update_cart(cart_id):
+    """
+    Function:
+    update_cart(cart_id:):
+     - Description: Allows the user to update the quantity of a specific item in their cart. 
+    - Input: cart_id (integer) which represents the ID of the cart item to be updated.
+    - Output: Renders the update_cart template or redirects to the cart page after updating the quantity.
+    """
     curr_user = Users.get_user_by_email(session.get('user'))
     if curr_user == None:
         return redirect('/login')
@@ -43,6 +72,12 @@ def update_cart(cart_id):
     
 @cart_blueprint.route("/cart/remove_item/<int:cart_id>", methods=['GET', 'POST'])
 def remove_item(cart_id):
+    """
+    remove_item(cart_id:):
+    - Description: Allows the user to remove a specific item from their cart.
+    - Input: cart_id (integer) which represents the ID of the cart item to be removed.
+    - Output: Redirects the user to the cart page after removing the item.
+    """
     curr_user = Users.get_user_by_email(session.get('user'))
     if curr_user == None:
         return redirect('/login')
